038abcf50b89f7a4b8bdd43f2173b1f655f47c89
涼宮結音 <68138727+yuioto@users.noreply.github.com>
2022-07-18 16:36:20 +0800

    fix email.
    fix site.config.js email error.

--- config/site.config.js
+++ config/site.config.js
@@ -44,7 +44,7 @@ module.exports = {
   ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'yuiotochan@outlook.com',
+  email: 'mailto:yuiotochan@outlook.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
